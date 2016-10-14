3066d20be0915963a0650f50e387fb350f8ac8a3
blackoo00 <460544293@qq.com>
2016-10-14 17:28:13 +0800

    个人中心 正在修改日历页面

--- WgCms/Lib/Action/Wap/DistributionAction.class.php
+++ WgCms/Lib/Action/Wap/DistributionAction.class.php
@@ -16,9 +16,7 @@
 			return $arr2;
 		}
 		function register(){
-			$data = htmlspecialchars_decode(htmlspecialchars_decode($this->_post('data')));
-			// $data = htmlspecialchars_decode(htmlspecialchars_decode($this->_get('data')));
-			$data = $this->myUnserialize($data);
+			$data = $this->htmlSpecial('post');
 			$db = D('Account');
 			//基本验证
 			foreach ($data as $k => $v) {
@@ -75,6 +73,91 @@
 				$this->ajaxReturn($re,'注册成功',1);
 			}else{
 				$this->ajaxReturn($re,'注册失败',2);
+			}
+		}
+		function login(){
+			$data = $this->htmlSpecial('post');
+			$condition = array(
+				'username' => $data['username'],
+				'password' => md5($data['password']),
+			);
+			if(D('Account')->where($condition)->find()){
+				setcookie('zxg_login_user',$data['username'],time()+86400*30);
+				$this->ajaxReturn('','登陆成功',1);
+			}else{
+				$this->ajaxReturn('','账号或密码错误',2);
+			}
+		}
+		//解析js的JSON数据
+		function htmlSpecial($type){
+			switch ($type) {
+				case 'post':
+					$data = htmlspecialchars_decode(htmlspecialchars_decode($this->_post('data')));
+					break;
+				
+				default:
+					$data = htmlspecialchars_decode(htmlspecialchars_decode($this->_get('data')));
+					break;
+			}
+			$data = $this->myUnserialize($data);
+			return $data;
+		}
+		//获取个人信息
+		function myInfo(){
+			$account = $this->checkLogin('account');
+			$this->ajaxReturn($account,'',1);
+		}
+		//保存个人信息
+		function saveMyInfo(){
+			$data = $this->htmlSpecial('post');
+			$username = $this->checkLogin('username');
+			$re = D('Account')->where(array('username'=>$username))->save($data);
+			if($re){
+				$this->ajaxReturn('','修改成功',1);
+			}else{
+				$this->ajaxReturn('','修改失败',2);
+			}
+		}
+		//修改密码
+		function changePasswrod(){
+			$data = $this->htmlSpecial('post');
+			$account = $this->checkLogin('account');
+			//判断旧密码
+			if(md5($data['oldPassword']) != $account['password']){
+				$this->ajaxReturn('','旧密码不正确',1);
+			}
+			if($data['newPassword'] != $data['rePassword']){
+				$this->ajaxReturn('','两次密码输入不相同',1);
+			}
+			$re = D('Account')->where(array('username'=>$account['username']))->save(array('password'=>md5($data['newPassword'])));
+			if($re){
+				$this->ajaxReturn('','修改成功',1);
+			}else{
+				$this->ajaxReturn('','修改失败',2);
+			}
+		}
+		function loginOut(){
+			setcookie('zxg_login_user',null);
+			$this->ajaxReturn('','',1);
+		}
+		//判断登陆
+		function checkLogin($get = ''){
+			$username = $_COOKIE['zxg_login_user'];
+			$account = D('Account')->where(array('username'=>$username))->find();
+			if($account){
+				switch ($get) {
+					case 'account':
+						return $account;
+						break;
+					case 'username':
+						return $username;
+						break;
+					default:
+						return true;
+						break;
+				}
+			}else{
+				$this->ajaxReturn('','账号未登陆',-1);
 			}
 		}
 	}
---
+++ tpl/Wap/default/common/original/js/login_register.js
@@ -0,0 +1,58 @@
+$(document).on('click','.wg_form_message_wrap',function(){
+	$(this).find('input').focus();
+    $(this).addClass('wg_item_icon_input_active').siblings('.wg_form_message_wrap').removeClass('wg_item_icon_input_active');
+})
+$(document).on('click','#login-submit',function(){
+	var validation = $('.validation');
+	var val = 1;
+	validation.each(function(index, el) {
+		if($(this).val() == ''){
+			var warn = $(this).attr('placeholder');
+			 $.alert(warn);
+			val = 0;
+			return false;
+		}
+	});
+	if(val == 1){
+		var data = $('#login_form').serialize();
+		$.ajax({
+			url:'http://127.0.0.1/wg_original2/index.php?g=Wap&m=Distribution&a=login',
+			data:{data:data},
+			dataType:'json',
+			type:'post',
+			success:function(data){
+				if(data.status == 1){
+					location.href="Distribution_index.html";
+				}else{
+					$.alert(data.info);
+				}
+			}
+		})
+	}
+})
+
+$(document).on('click','#register-submit',function(){
+	var validation = $('.validation');
+	var val = 1;
+	validation.each(function(index, el) {
+		if($(this).val() == ''){
+			var warn = $(this).attr('placeholder');
+			 $.alert(warn);
+			val = 0;
+			return false;
+		}
+	});
+	if(val == 1){
+		var data = $('#register_from').serialize();
+		$.ajax({
+			url:'http://127.0.0.1/wg_original2/index.php?g=Wap&m=Distribution&a=register',
+			data:{data:data},
+			dataType:'json',
+			type:'post',
+			success:function(data){
+				location.href="login.html";
+				console.log(data);
+			}
+		})
+	}
+})
